f5e6f8665a392ed7db2e37952d1bf1a4b78a5261
manoranjith-shankar <21csbs023@skcet.ac.in>
2024-01-03 00:03:44 +0530

    configUpdate

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract 0xToken is ERC20 {
+contract Crypto0x is ERC20 {
     // Initialize contract with 1 million tokens minted to the creator of the contract
-    constructor() ERC20("0xToken", "0xT") {
+    constructor() ERC20("Crypto0x", "CRX") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.20",
   networks: {
     sepolia: {
       url: process.env.RPC_URL,
